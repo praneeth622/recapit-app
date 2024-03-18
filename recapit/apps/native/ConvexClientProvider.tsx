f50f035297cb6a801f1c7cb197155ece5c03093a
jashwanth0712 <jashwanth0712@gmail.com>
2024-03-18 15:50:08 +0530

    added turborepo template for expo and clerk

---
+++ recapit/apps/native/ConvexClientProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL);
+
+export default function ConvexClientProvider({ children }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
